c91048618f438b7ad28bdccaf39da4bf959e8192
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-09-27 11:34:10 +0530

    Adding describe to the test case.

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import Greeting from './Greeting';
-
-test('renders Heloow World as a text', () => {
-    render(<Greeting/>);
-    const helloWorldElement = screen.getByText(/hellow world/i);
-    expect(helloWorldElement).toBeInTheDocument();
+describe("Greeting component", () => {
+    test('renders Heloow World as a text', () => {
+        render(<Greeting/>);
+        const helloWorldElement = screen.getByText(/hellow world/i);
+        expect(helloWorldElement).toBeInTheDocument();
+    })
 })
